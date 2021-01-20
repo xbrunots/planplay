ceebe798c53862ec6c259878437e22d2c9f70c85
Bruno Brito <xbrunots@gmail.com>
2021-01-20 14:26:21 -0300

    init

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Plan Play | Planning Poker App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+         by @xbrunots 
+        </h1>
+  
+      </main>
+
+     
+    </div>
+  )
+}
