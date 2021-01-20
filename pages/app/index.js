af6928cfc35abcf2eeaabccaf6de6549a949476e
Bruno Brito <xbrunots@gmail.com>
2021-01-20 14:30:35 -0300

    okasoakso

--- pages/app/index.js
+++ pages/app/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function App() {
   return (
     <div className={styles.container}>
       <Head>
